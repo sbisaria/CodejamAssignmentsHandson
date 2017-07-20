fa91bbb7e7a1c9cf28e2b18e50be5cfb02e2f3be
tavdeskrent012 <sbisaria@tavisca.com>
2017-07-20 19:20:57 +0530

    out parameter demo

---
+++ OutParameterDemo/OutParameterDemo/Calculate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutParameterDemo
+{
+    public class Calculate
+    {
+
+        public void Addition(double first,double second, double third, double forth,out double additionResult)
+        {
+            additionResult = first + second + third + forth;
+        }
+
+        public void Substraction(double first, double second, double third, double forth,out double substractionResult)
+        {
+           substractionResult = first - second - third - forth;
+        }
+
+        public void Multiplication(double first, double second, double third, double forth,out double multiplicationResult)
+        {
+            multiplicationResult = first * second * third * forth;
+        }
+    }
+}
---
+++ OutParameterDemo/OutParameterDemo/InputOutput.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutParameterDemo
+{
+    public class InputOutput
+    {
+        public void Input()
+        {
+            Calculate calculate = new Calculate();
+            double firstNo,secondNo,thirdNo,forthNo, one,two,three;
+            Console.WriteLine("Enter the first No:");
+            bool isValidfirstNo = double.TryParse(Console.ReadLine(),out firstNo);
+
+            Console.WriteLine("Enter the second No:");
+            bool isValidsecondNo = double.TryParse(Console.ReadLine(), out secondNo);
+
+            Console.WriteLine("Enter the third No:");
+            bool isValidthirdNo = double.TryParse(Console.ReadLine(), out thirdNo);
+
+            Console.WriteLine("Enter the forth No:");
+            bool isValidforthNo = double.TryParse(Console.ReadLine(), out forthNo);
+
+            if (isValidfirstNo && isValidsecondNo && isValidthirdNo && isValidforthNo)
+            {
+                calculate.Addition(firstNo, secondNo, thirdNo, forthNo,out one);
+                calculate.Substraction(firstNo, secondNo, thirdNo, forthNo,out two);
+                calculate.Multiplication(firstNo, secondNo, thirdNo, forthNo,out three);
+                Console.WriteLine("Addition result: \t" + one + "\nSubstraction result:\t" + two + "\nMultiplication result:\t" + three);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");                
+            }
+        }
+    }
+}
